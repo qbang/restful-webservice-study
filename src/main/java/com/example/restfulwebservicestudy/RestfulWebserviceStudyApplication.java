9f43b1ef89d1a9007221844bc895e093b7a03524
qbang <vhfhddl0101@naver.com>
2022-01-17 18:08:20 +0900

    feat(all): initial commit

---
+++ src/main/java/com/example/restfulwebservicestudy/RestfulWebserviceStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulwebservicestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebserviceStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWebserviceStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restfulwebservicestudy/RestfulWebserviceStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulwebservicestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebserviceStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
