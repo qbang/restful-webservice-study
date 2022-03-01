19b11f89213d87b9a5517b6cf1e329ffacd60713
qbang <vhfhddl0101@naver.com>
2022-03-01 22:55:41 +0900

    feat: 국가별 리턴 메시지 핸들링

--- src/main/java/com/example/restfulwebservicestudy/RestfulWebserviceStudyApplication.java
+++ src/main/java/com/example/restfulwebservicestudy/RestfulWebserviceStudyApplication.java
@@ -2,12 +2,23 @@ package com.example.restfulwebservicestudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulWebserviceStudyApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestfulWebserviceStudyApplication.class, args);
+    }
+
+    @Bean
+    public SessionLocaleResolver localResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.KOREA);
+        return localeResolver;
     }
 
 }
