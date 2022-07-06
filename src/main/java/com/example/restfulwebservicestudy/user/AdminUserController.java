54a05d37a5604e73efd8e5767aaaac1eb39a1ae3
qbang <vhfhddl0101@naver.com>
2022-07-06 23:10:19 +0900

    feat: API 버전 관리

--- src/main/java/com/example/restfulwebservicestudy/user/AdminUserController.java
+++ src/main/java/com/example/restfulwebservicestudy/user/AdminUserController.java
@@ -3,6 +3,8 @@ package com.example.restfulwebservicestudy.user;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -36,8 +38,11 @@ public class AdminUserController {
         return mappingJacksonValue;
     }
 
-    @GetMapping("/users/{id}")
-    public MappingJacksonValue retrieveUser(@PathVariable int id) {
+//    @GetMapping("v1/users/{id}") -> url을 이용해서 버전 관리
+//    @GetMapping(value = "/users/{id}", params = "version=1") -> 파라미터를 이용해서 버전 관리
+//    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=1") -> 헤더를 이용한 버전 관리
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv1+json") // -> mime-type(헤더)를 이용한 버전 관리
+    public MappingJacksonValue retrieveUserV1(@PathVariable int id) {
         User user = userDaoService.findOne(id);
 
         if (user == null) {
@@ -50,6 +55,34 @@ public class AdminUserController {
         FilterProvider filterProvider = new SimpleFilterProvider().addFilter("UserInfo", filter);
 
         MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(user);
+        mappingJacksonValue.setFilters(filterProvider);
+
+        return mappingJacksonValue;
+    }
+
+//    @GetMapping("v2/users/{id}") -> url을 이용해서 버전 관리
+//    @GetMapping(value = "/users/{id}", params = "version=2") -> 파라미터를 이용해서 버전 관리
+//    @GetMapping(value = "/users/{id}", headers = "X-API-VERSION=2")  -> 헤더를 이용한 버전 관리
+    @GetMapping(value = "/users/{id}", produces = "application/vnd.company.appv1+json") // -> mime-type(헤더)를 이용한 버전 관리
+    public MappingJacksonValue retrieveUserV2(@PathVariable int id) {
+        User user = userDaoService.findOne(id);
+
+        if (user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        // User -> User2
+        UserV2 userV2 = new UserV2();
+        BeanUtils.copyProperties(user, userV2);
+
+        userV2.setGrade("VIP");
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id", "name", "joinDate", "ssn"); // 해당 필드만 가져오도록
+
+        FilterProvider filterProvider = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(userV2);
         mappingJacksonValue.setFilters(filterProvider);
 
         return mappingJacksonValue;
--- src/main/java/com/example/restfulwebservicestudy/user/User.java
+++ src/main/java/com/example/restfulwebservicestudy/user/User.java
@@ -1,10 +1,9 @@
 package com.example.restfulwebservicestudy.user;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
@@ -12,6 +11,7 @@ import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 //@JsonIgnoreProperties(value = {"password", "ssn"})
 @JsonFilter("UserInfo")
 public class User {
